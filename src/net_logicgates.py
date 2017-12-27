af4ce5687daf847beefafd67e0fe55d5861d02de
LukasCaha <lukas.caha@outlook.com>
2017-12-27 18:33:22 +0100

    back propagation demo

--- src/net_logicgates.py
+++ src/net_logicgates.py
@@ -4,15 +4,24 @@ import loss as l
 import random as r
 import basicfunctions as bf
 
+#net settings
+learnRate = 0.1
+batch = 1
 #layers
 inLayer = [0,0]
 compLayer = [0,0,0,0]
 outLayer = [0]
 #weights
-min = -5
-max = 5
+min = -50
+max = 50
 inToCompWeights = fp.randomWeights(len(inLayer)*len(compLayer),min,max)
+desiredInComp = []
+for d in range(0,len(inLayer)*len(compLayer)):
+	desiredInComp.insert(d,0)
 compToOutWeights = fp.randomWeights(len(compLayer)*len(outLayer),min,max)
+desiredCompOut = []
+for d in range(0,len(compLayer)*len(outLayer)):
+	desiredCompOut.insert(d,0)
 #statistic data
 lost = 100
 out = []
@@ -37,7 +46,7 @@ if(d==2):
 	
 dataLen = bf.minLenThree(inLayer1,inLayer2,outLayer1)
 
-for counter in range (0,100):
+for counter in range (0,1000):
 	#fwdprop
 	rnd = r.randint(0,dataLen-1)
 	inLayer = [inLayer1[rnd],inLayer2[rnd]]
@@ -46,3 +55,23 @@ for counter in range (0,100):
 	#loss
 	print(l.lossList(out,cor))
 	#backprop
+	for d in range(0,len(compLayer)*len(outLayer)):
+		desiredCompOut[d] -= (outLayer[0] - outLayer1[rnd]) * compToOutWeights[d]
+	for i in range(0,len(inLayer)):
+		for o in range(0,len(compLayer)):
+			desiredInComp[i*len(compLayer)+o] -= (compLayer[o] - compLayer[o]+desiredCompOut[i]) * inToCompWeights[i*len(compLayer)+o]
+	if(counter%batch==0):
+		for d in range(0,len(compLayer)*len(outLayer)):
+			compToOutWeights[d] += desiredCompOut[d] * learnRate / batch
+			if(compToOutWeights[d]>max):
+				compToOutWeights[d] = max 
+			if(compToOutWeights[d]<min):
+				compToOutWeights[d] = min 
+		for d in range(0,len(inLayer)*len(compLayer)):
+			inToCompWeights[d] += desiredInComp[d]* learnRate / batch
+			if(inToCompWeights[d]>max):
+				inToCompWeights[d] = max 
+			if(inToCompWeights[d]<min):
+				inToCompWeights[d] = min 
+			
+	
