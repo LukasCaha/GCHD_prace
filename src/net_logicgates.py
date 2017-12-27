14d58f0d972bc358d5b1df4cf041e0067e94091e
LukasCaha <lukas.caha@outlook.com>
2017-12-27 23:33:45 +0100

    plynuly posun do loss=0.5

--- src/net_logicgates.py
+++ src/net_logicgates.py
@@ -3,17 +3,19 @@ import fwdprop as fp
 import loss as l
 import random as r
 import basicfunctions as bf
+import matplotlib.pyplot as plt
+
 
 #net settings
-learnRate = 0.1
-batch = 1
+learnRate = 0.01
+batch = 4
 #layers
 inLayer = [0,0]
 compLayer = [0,0,0,0]
 outLayer = [0]
 #weights
-min = -50
-max = 50
+min = -50.0
+max = 50.0
 inToCompWeights = fp.randomWeights(len(inLayer)*len(compLayer),min,max)
 desiredInComp = []
 for d in range(0,len(inLayer)*len(compLayer)):
@@ -26,6 +28,7 @@ for d in range(0,len(compLayer)*len(outLayer)):
 lost = 100
 out = []
 cor = []
+lostStats = []
 #data
 d=0
 if(d==0):
@@ -47,19 +50,24 @@ if(d==2):
 dataLen = bf.minLenThree(inLayer1,inLayer2,outLayer1)
 
 for counter in range (0,1000):
+	for d in range(0,len(inLayer)*len(compLayer)):
+		desiredInComp[d] = 0
+	for d in range(0,len(compLayer)*len(outLayer)):
+		desiredCompOut[d] = 0
 	#fwdprop
-	rnd = r.randint(0,dataLen-1)
+	rnd = counter%dataLen-1 #r.randint(0,dataLen-1)
 	inLayer = [inLayer1[rnd],inLayer2[rnd]]
 	cor.insert(counter, outLayer1[rnd])
 	out.insert(counter, fp.forwardPropagation(inLayer,compLayer,outLayer,inToCompWeights,compToOutWeights)[0])
 	#loss
-	print(l.lossList(out,cor))
+	lostStats.insert(counter,l.lossList(out,cor))
+	#print(l.lossList(out,cor))
 	#backprop
 	for d in range(0,len(compLayer)*len(outLayer)):
 		desiredCompOut[d] -= (outLayer[0] - outLayer1[rnd]) * compToOutWeights[d]
 	for i in range(0,len(inLayer)):
 		for o in range(0,len(compLayer)):
-			desiredInComp[i*len(compLayer)+o] -= (compLayer[o] - compLayer[o]+desiredCompOut[i]) * inToCompWeights[i*len(compLayer)+o]
+			desiredInComp[i*len(compLayer)+o] -= (compLayer[o] - (compLayer[o]+desiredCompOut[o])) * inToCompWeights[i*len(compLayer)+o]
 	if(counter%batch==0):
 		for d in range(0,len(compLayer)*len(outLayer)):
 			compToOutWeights[d] += desiredCompOut[d] * learnRate / batch
@@ -74,4 +82,6 @@ for counter in range (0,1000):
 			if(inToCompWeights[d]<min):
 				inToCompWeights[d] = min 
 			
+plt.plot(lostStats)
+plt.show()
 	
