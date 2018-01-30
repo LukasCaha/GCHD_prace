9e3e72c62a511337f6e83998b79523d2e02d7b65
Lukáš Caha <lukas.caha@outlook.com>
2018-01-30 09:10:49 +0100

    CNN done

--- src/cnn.py
+++ src/cnn.py
@@ -62,8 +62,88 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             batch_size = 32,
                                             class_mode = 'binary')
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+
+#import tensorflow as tf
+#with tf.device('/device:GPU:0'):
+
+
+classifier.fit_generator(training_set,steps_per_epoch = 8000,epochs = 25,validation_data = test_set,validation_steps = 2000)
+
+
+
+from keras.models import load_model
+
+classifier.save('CNN_dog_cat')  # creates a HDF5 file 'my_model.h5'
+classifier.save_weights('weights.h5')
+
+
+
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_6.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+    
+    
+    
+# CPU vs GPU benchmark
+"""
+from __future__ import print_function
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import time
+
+def get_times(maximum_time):
+
+    device_times = {
+        "/gpu:0":[],
+        "/cpu:0":[]
+    }
+    matrix_sizes = range(500,50000,1)
+
+    for size in matrix_sizes:
+        for device_name in device_times.keys():
+
+            print("####### Calculating on the " + device_name + " #######")
+
+            shape = (size,size)
+            data_type = tf.float16
+            with tf.device(device_name):
+                r1 = tf.random_uniform(shape=shape, minval=0, maxval=1, dtype=data_type)
+                r2 = tf.random_uniform(shape=shape, minval=0, maxval=1, dtype=data_type)
+                dot_operation = tf.matmul(r2, r1)
+
+
+            with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
+                    start_time = time.time()
+                    result = session.run(dot_operation)
+                    time_taken = time.time() - start_time
+                    print(result)
+                    device_times[device_name].append(time_taken)
+
+            print(device_times)
+
+            if time_taken > maximum_time:
+                return device_times, matrix_sizes
+
+
+device_times, matrix_sizes = get_times(1.5)
+gpu_times = device_times["/gpu:0"]
+cpu_times = device_times["/cpu:0"]
+
+plt.figure(figsize=(20,10))
+plt.plot(matrix_sizes[:len(gpu_times)], gpu_times, 'o-', color='green')
+plt.plot(matrix_sizes[:len(cpu_times)], cpu_times, 'o-', color='blue')
+plt.ylabel('Time')
+plt.xlabel('Matrix size')
+plt.show()
+"""
