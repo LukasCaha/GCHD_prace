0569bead30b0332a72c9b74fe47e318e4122c351
LukasCaha <lukas.caha@outlook.com>
2018-02-19 19:51:49 +0100

    95%

--- src/cnn.py
+++ src/cnn.py
@@ -76,13 +76,13 @@ from keras.models import load_model
 classifier.save('CNN_dog_cat')  # creates a HDF5 file 'my_model.h5'
 classifier.save_weights('weights.h5')
 
-
+classifier = load_model('classifierNEW')
 
 
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('IMG_20180130_083902.jpg', target_size = (64, 64))
+test_image = image.load_img('Vitezslav_Jahn.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
